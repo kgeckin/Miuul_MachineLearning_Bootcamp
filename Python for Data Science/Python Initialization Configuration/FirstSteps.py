37b7ddf00d7c31c759e0685551c32a3850d95ff7
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-07 14:20:44 +0300

    Create FirstSteps.py

---
+++ Python for Data Science/Python Initialization Configuration/FirstSteps.py
@@ -0,0 +1,22 @@
+# print() ekrana yazı yazmayı sağlar.
+print("Hello World")
+
+# Karakter Dizileri(Strings) ve Sayılar(Numbers)
+print(9)
+print("Hello AI Era")
+
+# type() fonksiyonu nesnenin veri tipini bulmayı sağlar (int, string, float, bool)
+print(type(9))
+print(type(9.2))
+print(type("Berkcan"))
+
+# Assignments & Variables
+a = 9
+print(a)
+b = "Hello AI Era"
+print(b)
+c = 10
+print(a * c)
+print(a * 10)
+d = a - c
+print(d)

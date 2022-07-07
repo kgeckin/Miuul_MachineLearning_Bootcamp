9acb4e0645b89d249aba431acf3adf7f6c8af3ca
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-07 17:22:45 +0300

    Update hi.py

--- Python for Data Science/Homework1/hi.py
+++ Python for Data Science/Homework1/hi.py
@@ -1,9 +1,9 @@
-# 1 - Create hi.py ✓
+# 1 - Create hi.py ✓ 
 
-# 2 - Print hello and your name/surname ✓
+# 2 - Print hello and your name/surname in the hi.py ✓
+print("Hello, I'm Kardelen Geçkin!")
 
 # 3 - Open the terminal ✓
 
 # 4 - Run the codes in the terminal ✓
-
-print("Hello, I'm Kardelen Geçkin")
+print hi.py

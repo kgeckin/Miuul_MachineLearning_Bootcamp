411c397f66056b35469aed98717749bd6c6b7738
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-07 14:17:29 +0300

    Create VirtualEnvironment&PackageManagement.py

---
+++ Python for Data Science/Python Initialization Configuration/VirtualEnvironment&PackageManagement.py
@@ -0,0 +1,50 @@
+####################################################################
+# CONDA
+####################################################################
+
+# Listing Virtual Environement
+# conda env list
+
+# Creating Virtual Environment
+# conda create -n isim
+
+# Activating Virtual Environment
+# conda activate isim
+
+# Deactiaveting
+# conda deactive
+
+# Package in the env
+# conda list
+
+# Package Install
+# conda install isim
+# conda install isim1 isim2 isim3
+
+# Package Deleting
+# conda remove isim
+
+# Package install version
+# conda install isim=1.20.1
+
+# Upgrade package
+# conda upgrade isim
+
+# All package upgrade
+# conda upgrade -all
+
+#Exporting conda
+#conda env export > environment.yaml 
+
+#####################################################################
+# PIP
+#####################################################################
+
+# pip:pypi (python package install)
+
+# Installing
+# pip install isim
+
+# Installing version
+# pip install isim==1.20.2
+

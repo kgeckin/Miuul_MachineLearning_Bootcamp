db7598580b2740ec27e6335f607cdf2474508dd0
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-07 14:39:00 +0300

    Update DataStructure.py

--- Python for Data Science/Data Structure/DataStructure.py
+++ Python for Data Science/Data Structure/DataStructure.py
@@ -1,1 +1,44 @@
+# Data Structer
 
+# Integer
+x = 46
+print(x)
+print(type(x))
+
+# Float 
+y = 10.3
+print(y)
+print(type(y))
+
+# Complex
+z = 2i+j
+print(z)
+print(type(z))
+
+# String 
+a = "Hello ai era"
+print(a)
+print(type(a))
+
+# Boolean (True/False)
+print(5 == 4)
+print(3 == 2)
+
+# List
+b = ["a","b","c"]
+print(type(b))
+
+# Dictionary = {"key" : "value", "key" : value } 
+c = {"name" : "Berkcan","Age":23}
+print(c)
+print(type(c))
+
+# Tuple
+d = ("python","ml","ds")
+print(d)
+print(type(d))
+
+# Set 
+e = {"python","ml","ds"}
+print(e)
+print(type(e))

b6efecd4b2ddffcc4bc28e95cb5724e00414b368
Kardelen Geçkin <101407832+kgeckin@users.noreply.github.com>
2022-07-07 14:40:42 +0300

    Create Numbers.py

---
+++ Python for Data Science/Data Structure/Numbers.py
@@ -0,0 +1,19 @@
+# int, float, complex
+
+a = 5
+b = 10.5
+
+print(a * 3)
+print(a/ 7)
+print(a * b /10)
+print(a ** 2) # a^2
+
+
+# Tip Dönüşümü
+
+print(int(b))
+print(float(a))
+print(int(a * b / 10))
+
+c = a * b / 10
+print(int(c))
